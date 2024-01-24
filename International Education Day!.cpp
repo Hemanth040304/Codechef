cca4241a703a49ad62a5ede630b691091988fb28
Hemanth <91045353+Hemanth040304@users.noreply.github.com>
2024-01-24 22:54:50 +0530

    Starters 118

---
+++ International Education Day!.cpp
@@ -0,0 +1,11 @@
+//https://www.codechef.com/problems/IED?tab=statement
+//Starters 118
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a,b,c;
+	cin>>a>>b>>c;
+	cout<<max(a*c,b*c);
+}
